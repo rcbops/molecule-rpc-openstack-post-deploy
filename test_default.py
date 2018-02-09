185a9f3846061c512f928bb4056d3a73cd5c94aa
John Duarte <john.duarte@rackspace.com>
2018-02-09 07:38:39 -0800

    Basic ntp deploy and test
    Note that the verify step will have failure due to docker
    not being able to manage services in the container. However,
    the mechanics of create, provision, and verify work as
    expected.

--- test_default.py
+++ test_default.py
@@ -6,9 +6,27 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_ntp_package(host):
+    assert host.package("ntp").is_installed
 
+
+def test_ntp_file(host):
+    f = host.file("/etc/ntp.conf")
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.is_file
+
+
+def test_ntp_file_content(host):
+    f = host.file("/etc/ntp.conf")
+    assert f.contains("0.us.pool.ntp.org")
+
+
+def test_ntp_service(host):
+    if host.system_info.distribution == "debian":
+        ntp_daemon = "ntp"
+    elif host.system_info.distribution == "centos":
+        ntp_daemon = "ntpd"
+
+    s = host.service(ntp_daemon)
+    assert s.is_running
+    assert s.is_enabled
