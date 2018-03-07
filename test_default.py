4fb09cc7e497b26b0f8f3240f3bb5992429e52b4
phongdly <phongdly@gmail.com>
2018-03-07 14:09:21 -0800

    update playbook to run only on compute-infra_host

--- test_default.py
+++ test_default.py
@@ -6,26 +6,27 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('compute-infra_hosts')
 
-pre_cmd = "sudo bash -c \"source /root/openrc; "
-conf_files = ['/etc/openstack_deploy/user_variables.yml,' \
+conf_files = ['/etc/openstack_deploy/user_variables.yml',
               '/etc/ansible/roles/os_nova/defaults/main.yml']
 
 
-def verify_nova_force_config_drive_is_disabled(host):
-    for searched_file in conf_files:
-        # Check nova_force_config_drive setting in /etc/openstack_deploy/user_variables.yml
-        cmd = pre_cmd + "pushd /opt/openstack-ansible; " \
-                        "ansible compute_hosts -m shell -a " \
-                        "'grep nova_force_config_drive'" + searched_file + "\""
-        # testinfra file contain something then
-        output = host.check_output(cmd)
-
-        if output:
+def test_nova_force_config_drive_is_disabled(host):
+    for conf_file in conf_files:
+        if host.file(conf_file).contains('nova_force_config_drive'):
+            cmd = "grep nova_force_config_drive " + conf_file
+            output = host.check_output(cmd)
             # Fail test if there is 'nova_force_config_drive: True'
             assert not (re.search('nova_force_config_drive:\s+True', output))
             # Fail test if there is 'nova_force_config_drive: none'
             assert not (re.search('nova_force_config_drive:\s+none', output))
             # Verify the 'nova_force_config_drive' is set to be False
-            assert (re.serch('nova_force_config_drive:\s+False', output))
+            assert (re.search('nova_force_config_drive:\s+False', output))
 
-    host.run('popd')
+
+def test_haproxy_packages_purged(host):
+    assert host.run_expect(1, 'dpkg -l | grep haproxy')
+
+
+def test_haproxy_service_not_running(host):
+    output = host.check_output('service haproxy status')
+    assert (re.search('Active:\s+inactive', output))
