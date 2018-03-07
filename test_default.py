f61620061158cca8c246c78419c7291a9a303218
phongdly <phongdly@gmail.com>
2018-03-07 12:20:50 -0800

    updated tests and required files/directories
    (cherry picked from commit d04fa6600bdba2fc7762b20d8509646b9d6aba7a)

--- test_default.py
+++ test_default.py
@@ -1,14 +1,31 @@
 import os
+import re
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('compute_hosts')
+
+pre_cmd = "sudo bash -c \"source /root/openrc; "
+conf_files = ['/etc/openstack_deploy/user_variables.yml,' \
+              '/etc/ansible/roles/os_nova/defaults/main.yml']
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def verify_nova_force_config_drive_is_disabled(host):
+    for searched_file in conf_files:
+        # Check nova_force_config_drive setting in /etc/openstack_deploy/user_variables.yml
+        cmd = pre_cmd + "pushd /opt/openstack-ansible; " \
+                        "ansible compute_hosts -m shell -a " \
+                        "'grep nova_force_config_drive'" + searched_file + "\""
+        # testinfra file contain something then
+        output = host.check_output(cmd)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+        if output:
+            # Fail test if there is 'nova_force_config_drive: True'
+            assert not (re.search('nova_force_config_drive:\s+True', output))
+            # Fail test if there is 'nova_force_config_drive: none'
+            assert not (re.search('nova_force_config_drive:\s+none', output))
+            # Verify the 'nova_force_config_drive' is set to be False
+            assert (re.serch('nova_force_config_drive:\s+False', output))
+
+    host.run('popd')
