8156f93d7900318ffbf4b4daf2b139200e823cbb
phongdly <phongdly@gmail.com>
2018-03-07 07:38:13 -0800

    (maint) run shell cli directly on storage host

--- test_default.py
+++ test_default.py
@@ -7,25 +7,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('cinder_volume')
 
 
-def test_cinder_service(host):
+def test_cinder_service(skiptesthost):
     cmd = "sudo bash -c \"source /root/openrc; cinder service-list\""
-    output = host.run(cmd)
+    output = skiptesthost.run(cmd)
     assert ("cinder-volume" in output.stdout)
 
 
 def test_cinder_lvm_volume(host):
-    cmd = "sudo bash -c \"source /root/openrc; " \
-          "pushd /opt/openstack-ansible; " \
-          "ansible storage_hosts -m shell -a 'vgs cinder-volumes'\""
-    output = host.run(cmd)
+    output = host.run('vgs cinder-volumes')
     assert re.search("cinder-volumes\s+[0-9]*\s+[0-9]*\s+", output.stdout)
 
 
 def test_cinder_volume_group(host):
-    cmd = "sudo bash -c \"source /root/openrc; " \
-          "pushd /opt/openstack-ansible; " \
-          "ansible cinder_volume -m shell -a " \
-          "'grep volume_group /etc/cinder/cinder.conf'\""
-    output = host.run(cmd)
+    output = host.run('grep volume_group /etc/cinder/cinder.conf')
     assert ("SUCCESS" in output.stdout)
     assert ("volume_group=cinder-volumes" in output.stdout)
