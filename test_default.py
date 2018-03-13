687e87c5cad3f0539db1e68dce36d60fca3663ae
phongdly <phongdly@gmail.com>
2018-03-13 15:19:38 -0700

    updated document using docstrings and marking jira tickets

--- test_default.py
+++ test_default.py
@@ -7,51 +7,97 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('cinder_volume')
 
 
-# TODO: will update the test to run cinder command on controller node
-# TODO: since the test cannot run on storage hosts.
+@pytest.mark.jira('asc-222')
 @pytest.mark.skip(reason='this test can not run on storage host')
 def test_cinder_service(host):
+    """
+    TODO: will update the test to run cinder command on controller node
+    TODO: since the test cannot run on storage hosts.
+    """
     cmd = "sudo bash -c \"source /root/openrc; cinder service-list\""
     output = host.run(cmd)
     assert ("cinder-volume" in output.stdout)
 
 
-##########################################################################
-# ASC-222: Verify the Filesystems were correctly provisioned for Cinder  #
-##########################################################################
-
-# Test 2a: Check the Cinder Nodes
+@pytest.mark.jira('asc-222')
 def test_cinder_lvm_volume(host):
+    """Test 2a: Check the Cinder Nodes: volume group
+    Args:
+        host(string): A hostname in dynamic_inventory.json/molecule.yml
+    Returns:
+        int: Exit_codes (test failed if exit_code is not zero)
+    """
+
     output = host.run('vgs cinder-volumes')
     assert re.search("cinder-volumes\s+[0-9]*\s+[0-9]*\s+", output.stdout)
 
 
+@pytest.mark.jira('asc-222')
 def test_cinder_volume_group(host):
+    """Test 2a: Check the Cinder config file
+    Args:
+        host(string): A hostname in dynamic_inventory.json/molecule.yml
+    Returns:
+        int: Exit_codes (test failed if exit_code is not zero)
+    """
+
     assert host.file('/etc/cinder/cinder.conf').contains("volume_group")
 
 
-# Test 2b:
-# Check Compute hosts LVM setup and verify we're not leaving an excess of
-# Free Extents.
-# Notes: makes sure if you are using LVM for your partitioning, that all
-# available disk space, past the amount needed for the OS, has been allocated
-# to /var/lib/nova
-# TODO: 1. This test is only for bare metal
-# TODO: 2. This test must be executed on compute_hosts (not storage_hosts)
+@pytest.mark.jira('asc-222')
 @pytest.mark.skip(reason='This test is only for bare metal')
 def test_list_lxc_volume_group(host):
+    """Test 2b:
+    TODO: 1. This test is only for bare metal
+    TODO: 2. This test must be executed on compute_hosts (not storage_hosts)
+    Check Compute hosts LVM setup and verify we're not leaving an excess of free extents.
+    Notes: makes sure if you are using LVM for your partitioning, that all
+    available disk space, past the amount needed for the OS, has been allocated
+    to /var/lib/nova
+
+    Args:
+        host(string): A hostname in dynamic_inventory.json/molecule.yml
+    Returns:
+        int: Exit_codes (test failed if exit_code is not zero)
+    """
+
     assert host.run_expect([0], 'vgs lxc')
     # More assertions need to be implemented here
 
 
+@pytest.mark.jira('asc-222')
 @pytest.mark.skip(reason='This test is only for bare metal')
 def test_list_lxc_logical_volume(host):
+    """Test 2b:
+    Check Compute hosts LVM setup and verify we're not leaving an excess of free extents.
+    Notes: makes sure if you are using LVM for your partitioning, that all
+    available disk space, past the amount needed for the OS, has been allocated
+    to /var/lib/nova
+
+    Args:
+        host(string): A hostname in dynamic_inventory.json/molecule.yml
+    Returns:v
+        int: Exit_codes (test failed if exit_code is not zero)
+    """
+
     assert host.run_expect([0], 'lvs lxc')
     # More assertions need to be implemented here
 
 
+@pytest.mark.jira('asc-222')
 @pytest.mark.skip(reason='This test is only for bare metal')
 def test_list_free_extents(host):
+    """Test 2b:
+    Check Compute hosts LVM setup and verify we're not leaving an excess of free extents.
+    Notes: makes sure if you are using LVM for your partitioning, that all
+    available disk space, past the amount needed for the OS, has been allocated
+    to /var/lib/nova
+
+    Args:
+        host(string): A hostname in dynamic_inventory.json/molecule.yml
+    Returns:
+        int: Exit_codes (test failed if exit_code is not zero)
+    """
     cmd = "vgs -o -pv_count,lv_count,snap_count,vg_attr,vg_size,vg_free " \
           "-o +vg_free_count"
     output = host.run(cmd)
@@ -60,17 +106,25 @@ def test_list_free_extents(host):
     assert ("not many extented found" in output)
 
 
-# Test 2c:
-# Check /etc/openstack_deploy/openstack_user_config.yml to make sure
-# Cinder is using proper 'cinder-volumes' group, and not 'lxc'
-# In other word: just makes sure that if a host has two scsi devices,
-# a small one for lxc (lxc group usually) and a larger one for
-# cinder-volumes group On actual hardware the 1st scsi device is for
-# the OS and configured as RAID 1 where the rest is RAID 10 for compute
-# nodes and cinder nodes in order to maximize disk space and IO
-# TODO: 1. This test is only for bare metal
-# TODO: 2. This test must be executed on compute_hosts (not storage_hosts)
+@pytest.mark.jira('asc-222')
 @pytest.mark.skip(reason='This test is only for bare metal')
 def test_check_cinder_volumes_in_user_config_file(host):
+    """Test 2c:
+    TODO: 1. This test is only for bare metal
+    TODO: 2. This test must be executed on compute_hosts (not storage_hosts)
+    Check /etc/openstack_deploy/openstack_user_config.yml to make sure
+    Cinder is using proper 'cinder-volumes' group, and not 'lxc'
+    In other word: just makes sure that if a host has two scsi devices,
+    a small one for lxc (lxc group usually) and a larger one for
+    cinder-volumes group On actual hardware the 1st scsi device is for
+    the OS and configured as RAID 1 where the rest is RAID 10 for compute
+    nodes and cinder nodes in order to maximize disk space and IO
+
+    Args:
+        host(string): A hostname in dynamic_inventory.json/molecule.yml
+    Returns:
+        int: Exit_codes (test failed if exit_code is not zero)
+    """
+
     assert host.file('/etc/openstack_deploy/openstack_user_config.yml')\
         .contains('something')
