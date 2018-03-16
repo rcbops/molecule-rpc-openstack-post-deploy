b1e5c498d1439e6d2556c76aea728e32a0f83222
phongdly <phongdly@gmail.com>
2018-03-16 15:35:43 -0700

    replaced dashboard_hosts by only infra1

--- test_dashboard.py
+++ test_dashboard.py
@@ -4,7 +4,7 @@ import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('dashboard_hosts')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('infra1')
 
 
 @pytest.mark.jira('asc-222')
