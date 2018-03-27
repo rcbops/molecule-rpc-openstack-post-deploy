da1020e7ee1db2d569354a4b0cc9329248197e09
phongdly <phongdly@gmail.com>
2018-03-27 16:00:08 -0700

    asc-239 Verify the correct networks were created
    (cherry picked from commit 9e122c518e2ae9e07ee666c9ec50e2f7bd0e5342)

--- test_default.py
+++ test_default.py
@@ -1,14 +1,32 @@
 import os
-
+import re
+import pytest
 import testinfra.utils.ansible_runner
 
+#######################################################################
+# ASC-239: Verify the correct networks were created                   #
+#######################################################################
+# RPC 10+ manual test 9
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('compute1')
+
+pre_cmd = "bash -c \"source /root/openrc; "
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.jira('asc-239')
+def test_verify_network_list(host):
+    """Verify the neutron network was created"""
+    cmd = pre_cmd + "openstack network list\""
+    output = host.run(cmd)
+    assert ("GATEWAY_NET" in output.stdout)
+    assert ("PRIVATE_NET" in output.stdout)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+@pytest.mark.jira('asc-239')
+def test_verify_subnet_list(host):
+    """Verify the neutron subnet was created """
+    cmd = pre_cmd + "openstack subnet list\""
+    output = host.run(cmd)
+    assert ("GATEWAY_NET_SUBNET" in output.stdout)
+    assert ("PRIVATE_NET_SUBNET" in output.stdout)
