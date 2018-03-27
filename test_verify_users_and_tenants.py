ab6a24baf2073a26b674c2fc899866855d78c319
phongdly <phongdly@gmail.com>
2018-03-27 11:07:49 -0700

    asc-236 verify users and tenant were created

---
+++ test_verify_users_and_tenants.py
@@ -0,0 +1,42 @@
+import os
+import re
+import pytest
+import testinfra.utils.ansible_runner
+
+#######################################################################
+# ASC-236: Verify the requested user(s) and tenant(s) were created    #
+# and assigned the proper role.                                       #
+#                                                                     #
+# Notes: These tests should be in keystone submodule, however, there  #
+# are not many test cases for keystone at this time so we are putting #
+# them here in novo submodule. When the keystone test cases grow in   #
+# the future, we should create keystone submodule for them            #
+#######################################################################
+# RPC 10+ manual test 7
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('compute1')
+
+pre_cmd = "bash -c \"source /root/openrc; "
+
+
+@pytest.mark.jira('asc-236')
+def test_keystone_users(host):
+    """Verify the requested users were created"""
+    cmd = pre_cmd + "openstack user list --domain=default\""
+    output = host.run(cmd)
+    assert ("cinder" in output.stdout)
+    assert ("glance" in output.stdout)
+    # assert ("maas" in output.stdout)
+    assert ("heat" in output.stdout)
+    assert ("keystone" in output.stdout)
+    assert ("neutron" in output.stdout)
+    assert ("nova" in output.stdout)
+
+
+@pytest.mark.jira('asc-236')
+def test_keystone_tenants(host):
+    """Verify the service tenant was created """
+    cmd = pre_cmd + "openstack project list\""
+    output = host.run(cmd)
+    assert ("service" in output.stdout)
