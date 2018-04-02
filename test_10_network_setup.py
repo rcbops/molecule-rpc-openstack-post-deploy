bb2ed708cf1e3427b25e3085481917fa85b1e73b
phongdly <phongdly@gmail.com>
2018-04-02 08:53:19 -0700

    (ASC-344) cleaned up the setup network test case

--- test_10_network_setup.py
+++ test_10_network_setup.py
@@ -7,26 +7,32 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('compute-infra_hosts')[:1]
 
+PLAYBOOK_DIR = "/opt/openstack-ansible-ops/multi-node-aio-xenial-ansible/playbooks/"
+SERVICE_YML = PLAYBOOK_DIR + "openstack-service-setup.yml"
+NETWORK_TEMP = PLAYBOOK_DIR + "/openstack-network-setup_reo198.yml"
+DYNAMIC_INV = "/opt/openstack-ansible/playbooks/inventory/dynamic_inventory.py"
+
+
 @pytest.mark.jira('asc-344')
 def test_setup_network(host):
     """Create network and subnet"""
     # work around REO-198:
-    host.run("cp /opt/openstack-ansible-ops/multi-node-aio-xenial-ansible/playbooks/openstack-service-setup.yml "
-             "/opt/openstack-ansible-ops/multi-node-aio-xenial-ansible/playbooks/openstack-network-setup_reo198.yml")
-    host.run("sed -i '/Create flavors of nova VMs/,/^$/d' "
-             "/opt/openstack-ansible-ops/multi-node-aio-xenial-ansible/playbooks/openstack-network-setup_reo198.yml")
-    output1 = host.run(
-        'cat /opt/openstack-ansible-ops/multi-node-aio-xenial-ansible/playbooks/openstack-network-setup.yml')
-    print ("cat openstack-network-setup.yml:" + output1.stdout)
+    cmd1 = "cp " + SERVICE_YML + " " + NETWORK_TEMP
+    host.run(cmd1)
+
+    # Remove task of creating flavors of Nova VMs
+    cmd2 = "sed -i '/Create flavors of nova VMs/,/^$/d' " + NETWORK_TEMP
+    host.run(cmd2)
 
     # Test to create network and subnet
-    output2 = host.run("export ANSIBLE_HOST_KEY_CHECKING=False ; "
-                       "cd /opt/openstack-ansible-ops/multi-node-aio-xenial-ansible/playbooks ; ansible-playbook -i "
-                       "/opt/openstack-ansible/playbooks/inventory/dynamic_inventory.py "
-                       "openstack-network-setup_reo198.yml")
-    print ("stdout:\n" + output2.stdout)
+    cmd3 = "export ANSIBLE_HOST_KEY_CHECKING=False ; " + "cd " + PLAYBOOK_DIR + " ; ansible-playbook -i " \
+           + DYNAMIC_INV + " openstack-network-setup_reo198.yml"
+    output = host.run(cmd3)
+    print ("stdout:\n" + output.stdout)
+
     # tear down, will be removed after REO-198 is fixed
-    host.run("rm -rf /opt/openstack-ansible-ops/multi-node-aio-xenial-ansible/playbooks/openstack-network-setup_reo198.yml")
+    cmd4 = "rm -rf " + NETWORK_TEMP
+    host.run(cmd4)
 
     # Verify ansible playbook run successfully to create network and subnet
-    assert not ('failed=1' in output2.stdout)
+    assert not ('failed=1' in output.stdout)
