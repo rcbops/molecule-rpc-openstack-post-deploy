424be9690c8f2de1d8cb8f6b0934b260503822b3
John Duarte <john.duarte@rackspace.com>
2018-04-11 16:50:23 -0700

    ASC-157 Use lxc-attach for dhcp agent
    This commit alters the dhcp agent test to use lxc-attach to the
    `utility` container for conducting `openstack` cli commands. The
    json format is also used to simplify the processing of the command
    results.

--- molecule/default/tests/test_dhcp_agents.py
+++ molecule/default/tests/test_dhcp_agents.py
@@ -1,31 +1,28 @@
 import os
 import testinfra.utils.ansible_runner
 import pytest
+import json
 
 """ASC-157: Perform Post Deploy System validations"""
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('dashboard_hosts')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('os-infra_hosts')[:1]
 
 
 @pytest.mark.jira('asc-157')
 def test_openvswitch(host):
     """
     Ensure DHCP agents for all networks are up
-
-    Assumes that results are returned in the following format:
-    +--------------+------------+------------+-------------------+-------+-------+--------------------+
-    | ID           | Agent Type | Host       | Availability Zone | Alive | State | Binary             |
-    +--------------+------------+------------+-------------------+-------+-------+--------------------+
-    | <network-id> | DHCP agent | <hostname> | nova              | :-)   | UP    | neutron-dhcp-agent |
-    +--------------+------------+------------+-------------------+-------+-------+--------------------+
     """
 
-    os_pre = '. /root/openrc ; '
-    net = os_pre + "openstack network list | awk '/[0-9]/ {print $2}'"
-    netres = host.run(net)
-    networks = netres.stdout.split('\n')
+    os_pre = ("lxc-attach -n $(lxc-ls -1 | grep utility | head -n 1) "
+              "-- bash -c '. /root/openrc ; openstack ")
+    net_cmd = "{} network list -f json'".format(os_pre)
+    net_res = host.run(net_cmd)
+    networks = net_res.stdout.split('\n')
+    networks = json.loads(net_res.stdout)
     for network in networks:
-        cmd = os_pre + 'openstack network agent list --network "' + network + '"'
+        cmd = "{} network agent list --network {} -f json'".format(os_pre,
+        network['ID'])
         res = host.run(cmd)
         assert "UP" in res.stdout
