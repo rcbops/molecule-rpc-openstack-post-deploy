dded97bf7cf3a16c60f70809bb693f87ffa218b2
phongdly <phongdly@gmail.com>
2018-04-11 13:56:40 -0700

    (ASC-240) updated with correct command
    (cherry picked from commit 2d6b1a086e0abd05ac49f1377f32c2afed4eddd9)

--- test_default.py
+++ test_default.py
@@ -11,8 +11,8 @@ RPC 10+ manual test 10
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('compute-infra_hosts')[:1]
 
-# attach the utility container on infra1
-pre_cmd = "lxc-attach -n `lxc-ls -1 | grep utility | head -n 1` -- bash -c "
+# attach the utility container:
+attach_utility_container = "lxc-attach -n `lxc-ls -1 | grep utility | head -n 1` -- bash -c "
 
 
 @pytest.mark.jira('asc-240')
@@ -20,7 +20,7 @@ def test_verify_glance_image(host):
     """Verify the glance images created by:
     https://github.com/openstack/openstack-ansible-ops/blob/master/multi-node-aio/playbooks/vars/openstack-service-config.yml
     """
-    cmd = pre_cmd + "'. /root/openrc ; openstack image list'"
+    cmd = attach_utility_container + "'. /root/openrc ; openstack image list'"
     output = host.run(cmd)
     assert ("Ubuntu 14.04 LTS" in output.stdout)
     assert ("Ubuntu 16.04" in output.stdout)
@@ -34,10 +34,10 @@ def test_verify_glance_image(host):
 
 @pytest.mark.jira('asc-240')
 def test_verify_subnet_list(host):
-        """Verify the VM flavor created by:
+    """Verify the VM flavor created by:
     https://github.com/openstack/openstack-ansible-ops/blob/master/multi-node-aio/playbooks/vars/openstack-service-config.yml
     """
-    cmd = pre_cmd + "'. /root/openrc ; openstack image list'"
+    cmd = attach_utility_container + "'. /root/openrc ; openstack flavor list'"
     output = host.run(cmd)
     assert ("m1.micro" in output.stdout)
     assert ("m1.tiny" in output.stdout)
