44c209889e1ebc7fb6aa031c5824de0872bdfdd4
phongdly <phongdly@gmail.com>
2018-04-12 14:06:13 -0700

    (ASC-255) Verify no more than 1 RabbitMQ channels per RabbitMQ connection

---
+++ test_verify_one_rabbitmp_channel_per_connection.py
@@ -0,0 +1,25 @@
+import os
+import pytest
+import testinfra.utils.ansible_runner
+
+
+"""ASC-255: Verify no more than 1 RabbitMQ channels per RabbitMQ connection
+
+RPC 10+ manual test 13
+"""
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('os-infra_hosts')[:1]
+
+# attach the rabbitMQ container:
+attach_rabbitmq_container = "lxc-attach -n `lxc-ls -1 | grep rabbit | head -n 1` -- "
+
+
+@pytest.mark.jira('asc-255')
+def test_verify_rabbitmq_channel_per_connection(host):
+    """Verify the glance images created by:
+    https://github.com/openstack/openstack-ansible-ops/blob/master/multi-node-aio/playbooks/vars/openstack-service-config.yml
+    """
+    cmd = attach_rabbitmq_container + "rabbitmqctl list_connections name channels | sort -nk 4 | tail -n1 | awk \'{print $4}\'"
+    output = host.run(cmd)
+    assert (output.stdout == '1')
