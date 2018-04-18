b8c68fa659fd1d2f40b438ec91ce8ef062802dbb
John Duarte <john.duarte@rackspace.com>
2018-04-18 10:57:17 -0700

    ASC-401 Add UUID values to tests
    This commit adds UUID marks to the current tests in order for them to be
    tracked by the test case management system.

--- test_cinder_service.py
+++ test_cinder_service.py
@@ -6,6 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('nova_compute')[:1]
 
 
+@pytest.mark.test_id('d7fc5630-432a-11e8-b9da-6a00035510c0')
 @pytest.mark.jira('asc-222')
 def test_cinder_service(host):
     """Test to verify that cinder service is running
