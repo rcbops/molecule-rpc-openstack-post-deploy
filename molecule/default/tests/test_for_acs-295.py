ca95e6466acffbc433762d64ef47e77166bbc076
Zach Reichert <zach.reichert@rackspace.com>
2018-05-15 09:26:35 -0700

    ASC-429 Add Jira @pytest.mark to flake8 linter

--- molecule/default/tests/test_for_acs-295.py
+++ molecule/default/tests/test_for_acs-295.py
@@ -14,30 +14,36 @@ class TestForRPC10PlusPostDeploymentQCProcessSwift(object):
 
     @pytest.mark.test_id('d7fc460f-432a-11e8-bd68-6a00035510c0')
     @pytest.mark.skip(reason='Need implementation')
+    @pytest.mark.jira('ASC-295')
     def test_verify_swift_ring_has_data(self, host):
         """See RPC 10+ Post-Deployment QC process document"""
 
     @pytest.mark.test_id('d7fc480f-432a-11e8-937e-6a00035510c0')
     @pytest.mark.skip(reason='Need implementation')
+    @pytest.mark.jira('ASC-295')
     def test_object_ring_rebalanced(self, host):
         """See RPC 10+ Post-Deployment QC process document"""
 
     @pytest.mark.test_id('d7fc49a8-432a-11e8-a2ea-6a00035510c0')
     @pytest.mark.skip(reason='Need implementation')
+    @pytest.mark.jira('ASC-295')
     def test_verify_md5_and_mounted_drives(self, host):
         """See RPC 10+ Post-Deployment QC process document"""
 
     @pytest.mark.test_id('d7fc4b42-432a-11e8-9ef9-6a00035510c0')
     @pytest.mark.skip(reason='Need implementation')
+    @pytest.mark.jira('ASC-295')
     def test_verify_swift_stat(self, host):
         """See RPC 10+ Post-Deployment QC process document"""
 
     @pytest.mark.test_id('d7fc4cdc-432a-11e8-a5dc-6a00035510c0')
     @pytest.mark.skip(reason='Need implementation')
+    @pytest.mark.jira('ASC-295')
     def test_verify_dispension_populate(self, host):
         """See RPC 10+ Post-Deployment QC process document"""
 
     @pytest.mark.test_id('d7fc4e61-432a-11e8-bcf5-6a00035510c0')
     @pytest.mark.skip(reason='Need implementation')
+    @pytest.mark.jira('ASC-295')
     def test_verify_dispension_report(self, host):
         """See RPC 10+ Post-Deployment QC process document"""
