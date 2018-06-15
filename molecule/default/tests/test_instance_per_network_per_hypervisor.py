6395e6dd61529414faeb17f90a9872ecf281de2c
John Duarte <john.duarte@rackspace.com>
2018-06-15 15:23:16 -0700

    Revert "ASC-572 Skip instance_per_network_per_hypervisor"
    This reverts commit 4e9391a7498927a195b173f3b64c24942e659338.

--- molecule/default/tests/test_instance_per_network_per_hypervisor.py
+++ molecule/default/tests/test_instance_per_network_per_hypervisor.py
@@ -43,7 +43,6 @@ def create_flavor_on(target_host, flavor):
                                                     os_post))
 
 
-@pytest.mark.skip(reason='ASC-639 No Ubuntu images in image list on Newton')
 @pytest.mark.test_id('c3002bde-59f1-11e8-be3b-6c96cfdb252f')
 @pytest.mark.jira('ASC-241')
 def test_hypervisor_vms(host):
