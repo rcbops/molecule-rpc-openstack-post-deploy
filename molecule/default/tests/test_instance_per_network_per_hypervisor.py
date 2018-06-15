577d652e84b2532565282c49330d7a323b02bdaf
John Duarte <john.duarte@rackspace.com>
2018-06-15 15:24:10 -0700

    ASC-636 Add assert for found images
    This commit adds an assertion that the found image list should not be
    zero prior to filtering for Ubuntu images.

--- molecule/default/tests/test_instance_per_network_per_hypervisor.py
+++ molecule/default/tests/test_instance_per_network_per_hypervisor.py
@@ -54,6 +54,7 @@ def test_hypervisor_vms(host):
     cmd = "{} image list -f json {}".format(os_pre, os_post)
     res = host.run(cmd)
     images = json.loads(res.stdout)
+    assert len(images) > 0
     filtered_images = list(filter(lambda d: 'buntu' in d['Name'], images))
     assert len(filtered_images) > 0
     image = filtered_images[-1]
