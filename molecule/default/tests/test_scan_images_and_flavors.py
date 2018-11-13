71e0cf83e34770355b7988c4114e507765f7f0c6
John Duarte <john.duarte@rackspace.com>
2018-11-13 09:09:15 -0800

    ASC-1030 Convert image/flavor validation to api

--- molecule/default/tests/test_scan_images_and_flavors.py
+++ molecule/default/tests/test_scan_images_and_flavors.py
@@ -1,6 +1,4 @@
-import os
 import pytest
-import testinfra.utils.ansible_runner
 
 
 """ASC-240: Verify the requested glance images were uploaded
@@ -8,44 +6,39 @@ import testinfra.utils.ansible_runner
 RPC 10+ manual test 10
 """
 
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('shared-infra_hosts')[:1]
-
-# attach the utility container:
-attach_utility_container = "lxc-attach -n `lxc-ls -1 | grep utility | head -n 1` -- bash -c "
-
 
 @pytest.mark.test_id('d7fc612b-432a-11e8-9a7a-6a00035510c0')
 @pytest.mark.jira('asc-240')
-def test_verify_glance_image(host):
+def test_verify_glance_image(os_api_connection):
     """Verify the glance images created by:
     https://github.com/openstack/openstack-ansible-ops/blob/master/multi-node-aio/playbooks/vars/openstack-service-config.yml
     """
-    cmd = attach_utility_container + "'. /root/openrc ; openstack image list'"
-    output = host.run(cmd)
-    assert ("Ubuntu 14.04 LTS" in output.stdout)
-    assert ("Ubuntu 16.04" in output.stdout)
-    assert ("Fedora 27" in output.stdout)
-    assert ("CentOS 7" in output.stdout)
-    assert ("OpenSuse Leap 42.3" in output.stdout)
-    assert ("Debian 9 Latest" in output.stdout)
-    assert ("Debian TESTING" in output.stdout)
-    assert ("Cirros-0.3.5" in output.stdout)
+    images = ['Ubuntu 14.04 LTS',
+              'Ubuntu 16.04',
+              'Fedora 27',
+              'CentOS 7',
+              'OpenSuse Leap 42.3',
+              'Debian 9 Latest',
+              'Debian TESTING',
+              'Cirros-0.3.5']
+    for image in images:
+        assert os_api_connection.get_image_name(image) == image
 
 
 @pytest.mark.test_id('d7fc62c7-432a-11e8-8102-6a00035510c0')
 @pytest.mark.jira('asc-240')
-def test_verify_vm_flavors(host):
+def test_verify_vm_flavors(os_api_connection):
     """Verify the VM flavor created by:
     https://github.com/openstack/openstack-ansible-ops/blob/master/multi-node-aio/playbooks/vars/openstack-service-config.yml
     """
-    cmd = attach_utility_container + "'. /root/openrc ; openstack flavor list'"
-    output = host.run(cmd)
-    assert ("m1.micro" in output.stdout)
-    assert ("m1.tiny" in output.stdout)
-    assert ("m1.mini" in output.stdout)
-    assert ("m1.small" in output.stdout)
-    assert ("m1.medium" in output.stdout)
-    assert ("m1.large" in output.stdout)
-    assert ("m1.xlarge" in output.stdout)
-    assert ("m1.heavy" in output.stdout)
+
+    flavors = ['m1.micro',
+               'm1.tiny',
+               'm1.mini',
+               'm1.small',
+               'm1.medium',
+               'm1.large',
+               'm1.xlarge',
+               'm1.heavy']
+    for flavor in flavors:
+        assert os_api_connection.get_flavor_name(flavor) == flavor
