c8f33d80bdec8504ce1f0596d8038dd19f316cd1
John Duarte <john.duarte@rackspace.com>
2018-11-13 09:00:15 -0800

    ASC-1030 Add openstack api fixture to tests dir
    This commit add the `os_api_connection` fixture to the tests directory
    to provide the facility for tests to validate against the openstack api
    directly rather than going through an additional client layer (e.g.
    python-openstackclient).

--- molecule/default/tests/conftest.py
+++ molecule/default/tests/conftest.py
@@ -1,4 +1,21 @@
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Imports
+# =============================================================================
 import pytest
+import openstack
+
+
+# =============================================================================
+# Fixtures
+# =============================================================================
+@pytest.fixture
+def os_api_connection():
+    """Provide an authorized API connection to the 'default' cloud on the OpenStack infrastructure.
+    Returns:
+        openstack.connection.Connection
+    """
+    return openstack.connect(cloud='default')
 
 
 @pytest.fixture
