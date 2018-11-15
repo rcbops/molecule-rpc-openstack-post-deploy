bb99d0dac177c0fc6d9230d11e29e36ec84046a6
John Duarte <john.duarte@rackspace.com>
2018-11-15 09:28:28 -0800

    ASC-1030 Use cli_host in create_and_assign_float
    This commit replaces the usage of `utility_container` with `cli_host` to
    execute `openstack` commands in `test_create_and_assign_floating_ip.py`

--- molecule/default/tests/test_create_and_assign_floating_ip.py
+++ molecule/default/tests/test_create_and_assign_floating_ip.py
@@ -1,13 +1,16 @@
-import pytest_rpc.helpers as helpers
+import pytest_rpc_helpers as helpers
 import os
 import pytest
 import testinfra.utils.ansible_runner
 
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('shared-infra_hosts')[:1]
+# TODO: Put these values into ansible facts
+cli_host = 'director'
+cli_openrc_path = '/home/stack/overcloudrc'
 
-utility_container = ("lxc-attach -n $(lxc-ls -1 | grep utility | head -n 1) "
-                     "-- bash -c '. /root/openrc ; ")
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts(cli_host)
+
+os_pre = ". {} ; ".format(cli_openrc_path)
 
 
 @pytest.mark.test_id('ab24ffbd-798b-11e8-a2b2-6c96cfdb2e43')
@@ -59,9 +62,9 @@ def test_assign_floating_ip_to_instance(openstack_properties, host):
 
     # Assigning floating ip to a server
     cmd = ("{} openstack server add "
-           "floating ip {} {}'".format(utility_container,
-                                       instance_id,
-                                       floating_ip)
+           "floating ip {} {}".format(os_pre,
+                                      instance_id,
+                                      floating_ip)
            )
     host.run_expect([0], cmd)
 
