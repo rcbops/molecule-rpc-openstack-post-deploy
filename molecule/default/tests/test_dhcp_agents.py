4f8c191953725c50e45f76731a49f9e8ce92d6a0
John Duarte <john.duarte@rackspace.com>
2018-11-15 08:53:04 -0800

    ASC-1030 Use cli_host in dhcp_agents
    This commit replaces the usage of `utility_container` with `cli_host` to
    execute `openstack` commands in `test_dhcp_agents.py`

--- molecule/default/tests/test_dhcp_agents.py
+++ molecule/default/tests/test_dhcp_agents.py
@@ -2,12 +2,17 @@ import os
 import testinfra.utils.ansible_runner
 import pytest
 import json
-import pytest_rpc.helpers as helpers
 
 """ASC-157: Perform Post Deploy System validations"""
 
+# TODO: Put these values into ansible facts
+cli_host = 'director'
+cli_openrc_path = '/home/stack/overcloudrc'
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('shared-infra_hosts')[:1]
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts(cli_host)
+
+os_pre = ". {} ; openstack ".format(cli_openrc_path)
 
 
 @pytest.mark.test_id('d7fc25ae-432a-11e8-a20a-6a00035510c0')
@@ -17,37 +22,16 @@ def test_openvswitch(host):
     Ensure DHCP agents for all networks are up
     """
 
-    r = host.ansible("setup")["ansible_facts"]["ansible_local"]["system_tests"]["rpc_product_release"]
-    expected_codename, expected_major = helpers.get_osa_version(r)
-    print "expected_major: {}".format(expected_major)
-    try:
-        osa_major = int(expected_major)
-    except ValueError:
-        osa_major = 99
-    print "osa_major: {}".format(osa_major)
-
-    os_pre = ("lxc-attach -n $(lxc-ls -1 | grep utility | head -n 1) "
-              "-- bash -c '. /root/openrc ; openstack ")
-    net_cmd = "{} network list -f json'".format(os_pre)
+    net_cmd = "{} network list -f json".format(os_pre)
     net_res = host.run(net_cmd)
     networks = net_res.stdout.split('\n')
     networks = json.loads(net_res.stdout)
     for network in networks:
-        if osa_major > 14:
-            net_agent_cmd = "{} network agent list --network {} -f json'".format(os_pre, network['ID'])
-        else:
-            net_agent_cmd = ("lxc-attach "
-                             "-n $(lxc-ls -1 | grep utility | head -n 1) "
-                             "-- bash -c '. /root/openrc ; "
-                             "neutron dhcp-agent-list-hosting-net {} "
-                             "-f json'".format(network['ID']))
+        net_agent_cmd = "{} network agent list --network {} -f json".format(os_pre, network['ID'])
 
         print net_agent_cmd
         res = host.run(net_agent_cmd)
         results = json.loads(res.stdout)
 
         for agent in results:
-            if osa_major > 14:
-                assert agent['State'] == 'UP'
-            else:
-                assert agent['admin_state_up'] is True
+            assert agent['State'] == 'UP'
