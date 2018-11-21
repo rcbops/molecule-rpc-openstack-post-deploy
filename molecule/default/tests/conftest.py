056182f2be08ab54e284f36eec4cb40052a0957f
Ryan Gard <ryan.gard@rackspace.com>
2018-11-21 14:20:05 -0800

    Revert "Merge pull request #131 from ryan-rs/maint/ASC-1082/provide_os_api_conn_fixture"
    This reverts commit 3a81693602828d0b9f427848bfefb4fd57c8dd1c, reversing
    changes made to d88d349fa41678332d911ac37fad8b7a62757dad.

--- molecule/default/tests/conftest.py
+++ molecule/default/tests/conftest.py
@@ -1,52 +1,17 @@
-# -*- coding: utf-8 -*-
-# ==============================================================================
-# Imports
-# ==============================================================================
 import pytest
-import openstack
 
 
-# ==============================================================================
-# Fixtures
-# ==============================================================================
-# TODO: these fixtures should be enhanced and moved to 'pytest-rpc'. (ASC-1253)
-@pytest.fixture(scope='module')
+@pytest.fixture
 def openstack_properties(host):
-    """This fixture returns a dictionary of OpenStack facts and variables from
-    Ansible which can be used to manipulate OpenStack objects. (i.e. create
-    server instances)
-
-    Args:
-        host(testinfra.host.Host): A hostname in dynamic_inventory.json or
-            molecule.yml
-
-    Returns:
-        dict: a static dictionary of data about OpenStack.
-    """
-
-    os_vars = host.ansible('include_vars',
-                           'file=./vars/main.yml')['ansible_facts']
+    """ this fixture returns a static dictionary of data used for creating
+    openstack server (instance)"""
+    vars = host.ansible('include_vars', 'file=./vars/main.yml')['ansible_facts']
 
     os_properties = {
         'image_name': 'Cirros-0.3.5',
-        'network_name': os_vars['gateway_network'],
-        'private_net': os_vars['private_network'],
-        'glance_nfs_share_path': os_vars['glance_nfs_share_path'],
-        'glance_nfs_mount_path': os_vars['glance_nfs_mount_path'],
+        'network_name': vars['gateway_network'],
+        'private_net': vars['private_network'],
         'flavor': 'm1.tiny',
         'zone': 'nova'
     }
-
     return os_properties
-
-
-@pytest.fixture(scope='session')
-def os_api_conn():
-    """Provide an authorized API connection to the 'default' cloud on the
-    OpenStack infrastructure.
-
-    Returns:
-        openstack.connection.Connection
-    """
-
-    return openstack.connect(cloud='default')
