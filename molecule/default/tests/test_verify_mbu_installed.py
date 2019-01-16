fcf330fdf9b2bafa966a878646024dbc2def2c25
Ryan Gard <ryan.gard@rackspace.com>
2019-01-16 14:32:04 -0800

    ASC-1331 Refactor 'test_verify_mbu_installed'
    The test logic is sound so the only changes have to do with implementing
    better code styling and docstrings. Ironically the test is now longer than
    it was previously!

--- molecule/default/tests/test_verify_mbu_installed.py
+++ molecule/default/tests/test_verify_mbu_installed.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ==============================================================================
 # Imports
 # ==============================================================================
@@ -5,6 +6,7 @@ import os
 import pytest
 import pytest_rpc.helpers as helpers
 import testinfra.utils.ansible_runner
+from pprint import pformat
 
 # ==============================================================================
 # Globals
@@ -13,29 +15,58 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('shared-infra_hosts')[:1]
 
 
-backup_folder_on_host = '/openstack/backup'
-backup_folder_on_container = '/var/backup'
+# ==============================================================================
+# Fixtures
+# ==============================================================================
+@pytest.fixture
+def backup_folder_path():
+    """Path of MBU backup folder for a given host type.
+
+    Returns:
+        dict: {'host', 'container'}
+    """
+
+    return {'host': '/openstack/backup', 'container': '/var/backup'}
 
 
 # ==============================================================================
 # Test Cases
 # ==============================================================================
 @pytest.mark.test_id('406be222-cbee-11e8-ae3a-0025227c8120')
-@pytest.mark.jira('asc-262')
+@pytest.mark.jira('ASC-262', 'ASC-1331')
 @pytest.mark.test_case_with_steps()
 class TestVerifyMBUInstall(object):
+    """Ensure that MBU backup paths are present which are prerequisites to
+    installing the MBU service.
+    """
 
-    def test_verify_backup_folder_on_host(self, host):
-        assert host.file(backup_folder_on_host).is_directory
+    def test_verify_backup_folder_on_host(self, host, backup_folder_path):
+        """Verify backup path on virtual/physical host.
 
-    def test_verify_backup_folder_on_container(self, host):
-        cmd1 = "ls {}".format(backup_folder_on_host)
+        Args:
+            host (testinfra.host.Host): Testinfra host fixture.
+            backup_folder_path (dict): Path of MBU backup folder for a given
+                host type.
+        """
+
+        assert host.file(backup_folder_path['host']).is_directory
+
+    def test_verify_backup_folder_on_container(self, host, backup_folder_path):
+        """Verify backup path on all container hosts.
+
+        Args:
+            host (testinfra.host.Host): Testinfra host fixture.
+            backup_folder_path (dict): Path of MBU backup folder for a given
+                host type.
+        """
+
+        cmd1 = "ls {}".format(backup_folder_path['host'])
         containers = host.run(cmd1).stdout.split('\n')
 
         # Fail the test if there is no container in the backup directory
-        print containers
-        assert (len(containers) > 1)
+        error_msg = "No containers found!\n{}".format(pformat(containers))
+        assert len(containers) > 1, error_msg
 
-        cmd2 = "test -d {}".format(backup_folder_on_container)
+        cmd2 = "test -d {}".format(backup_folder_path['container'])
         for container in containers:
             assert (helpers.run_on_container(cmd2, container, host))
