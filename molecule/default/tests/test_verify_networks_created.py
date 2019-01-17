f0f03bc8e509c5dd0fc1d98d97faac5b5dc79679
Ryan Gard <ryan.gard@rackspace.com>
2019-01-17 13:57:15 -0800

    ASC-1332 Fixes to Address Feedback

--- molecule/default/tests/test_verify_networks_created.py
+++ molecule/default/tests/test_verify_networks_created.py
@@ -14,15 +14,18 @@ import pytest
 # ==============================================================================
 @pytest.mark.test_id('d7fc646b-432a-11e8-b858-6a00035510c0')
 @pytest.mark.jira('ASC-239', 'ASC-1332')
-def test_verify_network_list(os_api_conn):
+def test_verify_network_list(os_api_conn, openstack_properties):
     """Verify that networks were created.
 
     Args:
         os_api_conn (openstack.connection.Connection): An authorized API
             connection to the 'default' cloud on the OpenStack infrastructure.
+        openstack_properties (dict): OpenStack facts and variables from Ansible
+            which can be used to manipulate OpenStack objects.
     """
 
-    expected_networks = ('GATEWAY_NET', 'PRIVATE_NET')
+    expected_networks = (openstack_properties['private_net'],
+                         openstack_properties['network_name'])
 
     for expected_network in expected_networks:
         assert os_api_conn.get_network(expected_network)
@@ -30,15 +33,18 @@ def test_verify_network_list(os_api_conn):
 
 @pytest.mark.test_id('d7fc65fa-432a-11e8-a2ae-6a00035510c0')
 @pytest.mark.jira('ASC-239', 'ASC-1332')
-def test_verify_subnet_list(os_api_conn):
+def test_verify_subnet_list(os_api_conn, openstack_properties):
     """Verify that subnets were created.
 
     Args:
         os_api_conn (openstack.connection.Connection): An authorized API
             connection to the 'default' cloud on the OpenStack infrastructure.
+        openstack_properties (dict): OpenStack facts and variables from Ansible
+            which can be used to manipulate OpenStack objects.
     """
 
-    expected_subnets = ('GATEWAY_NET_SUBNET', 'PRIVATE_NET_SUBNET')
+    expected_subnets = (openstack_properties['private_network_subnet'],
+                        openstack_properties['gateway_network_subnet'])
 
     for expected_subnet in expected_subnets:
         assert os_api_conn.get_subnet(expected_subnet)
