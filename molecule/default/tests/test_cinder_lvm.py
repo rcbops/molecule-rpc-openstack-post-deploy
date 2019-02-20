2065662c62af012a7b0b33b61b191ece78888d18
Ryan Gard <ryan.gard@rackspace.com>
2019-02-20 11:00:34 -0800

    ASC-1503 Use Snapshot Fixture
    Refactor test to use a snapshot fixture to guarantee teardown.

--- molecule/default/tests/test_cinder_lvm.py
+++ molecule/default/tests/test_cinder_lvm.py
@@ -15,13 +15,55 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 # ==============================================================================
+# Fixtures
+# ==============================================================================
+@pytest.fixture
+def create_volume_with_snapshot(os_api_conn,
+                                create_volume,
+                                openstack_properties):
+    """Create a test volume with a snapshot. (Automatic clean-up)
+
+    Args:
+        os_api_conn (openstack.connection.Connection): An authorized API
+            connection to the 'default' cloud on the OpenStack infrastructure.
+        create_volume (def): A factory function for generating volumes.
+        openstack_properties (dict): OpenStack facts and variables from Ansible.
+
+    Returns:
+        tuple: Return the volume and snapshot info (munch.Munch) upon successful
+            creation. (volume_info, snapshot_info)
+    """
+
+    # create volume
+    test_volume = create_volume(
+        size=1,
+        image=openstack_properties['cirros_image'],
+        bootable=False
+    )
+
+    # create a snapshot of the above newly created volume
+    snapshot_name = "test_volume_snapshot{}".format(
+        str(helpers.generate_random_string()))
+
+    test_snapshot = os_api_conn.create_volume_snapshot(volume_id=test_volume.id,
+                                                       name=snapshot_name,
+                                                       force=True,
+                                                       timeout=600)
+
+    yield test_volume, test_snapshot
+
+    # Teardown
+    os_api_conn.delete_volume_snapshot(name_or_id=test_snapshot.id, wait=True)
+
+
+# ==============================================================================
 # Test Cases
 # ==============================================================================
+# noinspection PyUnresolvedReferences,PyUnusedLocal
 @pytest.mark.jira('asc-157', 'asc-1503')
 @pytest.mark.test_id('b1e888fa-546a-11e8-9902-6c96cfdb252f')
 def test_cinder_lvs_volume_on_node(os_api_conn,
-                                   create_volume,
-                                   openstack_properties,
+                                   create_volume_with_snapshot,
                                    host):
     """Test to verify that a volume associated to a cinder host can be seen
         on that cinder host as a logical volume. If there is any snapshot
@@ -31,26 +73,10 @@ def test_cinder_lvs_volume_on_node(os_api_conn,
     Args:
         os_api_conn (openstack.connection.Connection): An authorized API
             connection to the 'default' cloud on the OpenStack infrastructure.
-        create_volume (def): A factory function for generating volumes.
-        openstack_properties (dict): OpenStack facts and variables from Ansible
+        create_volume_with_snapshot (tuple): Create a test volume with a
+            snapshot.
         host (testinfra.host.Host): Testinfra host fixture.
     """
-    # create volume:
-    test_volume = create_volume(
-        size=1,
-        image=openstack_properties['cirros_image'],
-        bootable=False
-    )
-
-    # create a snapshot of the above newly created volume
-    vol_id = test_volume.id
-    snapshot_name = "test_volume_snapshot{}".format(
-        str(helpers.generate_random_string()))
-
-    test_snapshot = os_api_conn.create_volume_snapshot(volume_id=vol_id,
-                                                       name=snapshot_name,
-                                                       force=True,
-                                                       timeout=600)
 
     for vol in os_api_conn.block_storage.volumes():
         chost = vol.to_dict()['host']
@@ -81,9 +107,3 @@ def test_cinder_lvs_volume_on_node(os_api_conn,
                     assert host.run(cmd).rc == 0, \
                         "The command: '{}' failed to run on" \
                         "host '{}'.".format(cmd, current_host)
-
-    # tear down
-    # tear down snapshot only, the volume tear down is handled in create_volume
-    # factory function
-    snapshot_id = test_snapshot.id
-    os_api_conn.delete_volume_snapshot(name_or_id=snapshot_id, wait=True)
